e071303db63078cd3a745ca819b1972ce711d011
Ruben Gill <rgill288@my.bcit.ca>
2023-11-03 22:05:09 -0700

    Added notes for how to solve the problem

--- lab6.c
+++ lab6.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 // Node
-typedef Node { 
-    ? data;
+typedef struct Node { 
+    int data;
     struct Node *link;
-}
+} Node;
 
 //Domestic student structure
-typedef struct {
+typedef struct DomesticStudent {
     char *firstName;
     char *lastName;
     struct DomesticStudent *link;
@@ -16,7 +16,7 @@ typedef struct {
 } DomesticStudent;
 
 //International student structure 
-typedef struct {
+typedef struct InternationalStudent {
     char *firstName;
     char *lastName;
     int toefl;
@@ -49,10 +49,32 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    fseek
+    fgets 
 
 
 
     return 0;
 
 }
+
+
+
+
+
+
+
+
+1. HOW TO SOLVE THE PROBLEM. WHAT IS THE PROBLEM ? 
+    - We need to READ the contents of a file, and then we need to WRITE the results to an output file. 
+    - THE PROBLEM IS: WRITE TO THE OUTPUT FILE: 
+        - STUDENTS WHOSE GPA IS GREATER THAN 3.9. Can store up TO 3 DECIMAL PLACES. STORE ORIGINAL DEC VALUE IF POSSIBLE
+        - INTERNATIONAL STUDENT HAS A TOEFL SCORE. IF LESS THAN 70 ( < 70) do NOT write to the output file 
+        - DOMESTIC STUDENT: NO TOEFL
+        - ORDER OF STUDENTS IS SAME AS ORDER IN THE FILE
+        - IF FORMAT OF STUDENT DOESNT FIT SPECIFIED FORMAT, OUTPUT APROPIATE ERROR MESSAGE
+        - OPTION FIELD: 1 = Save filtered output of domestic students, 2 = filtered output of inter students, 3 = output of all STUDENTS
+        - Assume everyone has a first and last name 
+        - every line ends in a new line character 
+        - WE NEED TO HANDLE CORNER CASES 
+2. READ in the input file using: fgets = RETURNS A STRING (NULL TERMINATED) FOR EACH LINE IN THE FILE. A LINE CORRESPONDS TO A STUDENT 
+3. 
