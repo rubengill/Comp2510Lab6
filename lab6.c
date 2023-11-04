f208764b9f2eb75a7c5095d200e7e8ff9cef1f65
Ruben Gill <rgill288@my.bcit.ca>
2023-11-03 17:07:19 -0700

    Added structures

--- lab6.c
+++ lab6.c
@@ -1,9 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct {
-    
+// Node
+typedef Node { 
+    ? data;
+    struct Node *link;
 }
+
+//Domestic student structure
+typedef struct {
+    char *firstName;
+    char *lastName;
+    struct DomesticStudent *link;
+    
+} DomesticStudent;
+
+//International student structure 
+typedef struct {
+    char *firstName;
+    char *lastName;
+    int toefl;
+    struct InternationalStudent *link;
+
+} InternationalStudent;
 
 int main(int argc, char *argv[]) {
 
@@ -15,6 +34,14 @@ int main(int argc, char *argv[]) {
     char *inputFileName = argv[1];
     char *outputFileName = argv[2];
     int option = atoi(argv[3]);
+
+    // Handle edge cases, gets written to the output file 
+    if (option < 0) {
+        printf("Error. Option must be greater than 3")
+    }
+
+    // If the in/out files are null / empty ? 
+    if
     
     FILE *fp = fopen(inputFileName, "r");
     if (!fp) { 
@@ -26,6 +53,6 @@ int main(int argc, char *argv[]) {
 
 
 
-    return 1;
+    return 0;
 
 }
