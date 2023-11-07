8e9270c631a44c7dd29873d3f01f7fbd0c96b918
Ruben Gill <rgill288@my.bcit.ca>
2023-11-06 22:18:29 -0800

    Added code to read in text from the input file, and check if each line has a new line character.

--- lab6.c
+++ lab6.c
@@ -1,33 +1,85 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-// Node
-typedef struct Node { 
-    int data;
-    struct Node *link;
-} Node;
+// Flag that we can use to determine what type of student 
+typedef enum {
+    DOMESTIC,
+    INTERNATIONAL
+} StudentType;
 
 //Domestic student structure
-typedef struct DomesticStudent {
+typedef struct {
     char *firstName;
     char *lastName;
-    struct DomesticStudent *link;
-    
+    double gpa;
 } DomesticStudent;
 
 //International student structure 
-typedef struct InternationalStudent {
+typedef struct {
     char *firstName;
     char *lastName;
+    double gpa;
     int toefl;
-    struct InternationalStudent *link;
-
 } InternationalStudent;
+
+// Defines a node in the linked list, which can either be a dStudent or iStudent
+typedef struct StudentNode {
+    // Flag to determine the type of student it is
+    StudentType type;
+
+    // Sets aside memory for an international student, which a domestic student can also occupy
+    union {
+        DomesticStudent dStudent;
+        InternationalStudent iStudent;
+    } student;
+
+    // Link to the next node
+    struct StudentNode *next;
+}
+
+// Ensure every line ends in a new line character 
+char* checkNewLine(char *buffer) {
+    // Number of elements before the new line 
+    int bufferElements = (int) strlen(buffer);
+
+    // 1 if has a new line
+    int hasNewLine = (bufferElements > 0 && buffer[bufferElements - 1] == '\n');
+
+    // Add another byte if does not contain a new line 
+    char *newArr = (char*)malloc((bufferElements + 1 + !hasNewLine)* sizeof(char));
+
+    // Copies bytes from the desired string, minus the null terminator
+    memcpy(newArr, buffer, bufferElements);
+
+    if (!hasNewLine) {
+        newArr[bufferElements] = '\n';
+        bufferElements++;
+    }
+
+    newArr[bufferElements] = '\0'
+
+    return newArr;
+}
+
+StudentNode *createStudentNode(char *arr, FILE *fp_out) {
+    StudentNode *newStudent = (StudentNode *)malloc(sizeof(StudentNode));
+
+    newStudent -> next = NULL;
+
+    if (arr == NULLL) {
+        printf("Error: There is no arr being passed in to create a student node !");
+    }
+
+    char *token = (arr, " ");
+}
+
+
 
 int main(int argc, char *argv[]) {
 
     if (argv != 4) {
-        printf("There must be 4 command line arguments");
+        printf("Error: There must be 4 command line arguments");
     }
 
     // Get command line arguments
@@ -35,26 +87,44 @@ int main(int argc, char *argv[]) {
     char *outputFileName = argv[2];
     int option = atoi(argv[3]);
 
-    // Handle edge cases, gets written to the output file 
-    if (option < 0) {
-        printf("Error. Option must be greater than 3")
-    }
-
-    // If the in/out files are null / empty ? 
-    if
-    
-    FILE *fp = fopen(inputFileName, "r");
-    if (!fp) { 
-        perror("Can't find the file pal!")
+    FILE *fp_out = fopen(outputFileName, "w");
+    if (!fp_out) {
+        perror("Error: Can't open the output file.");
+        fclose(fp);
         return 1;
     }
 
-    fgets 
+    // Gets written to the output file 
+    if (option < 1 || > 3) {
+        fprintf("Error: Option must be greater than 0");
+        fclose(fp_out_);
+        return 1;
+    }
+    
+    FILE *fp = fopen(inputFileName, "r");
+    if (!fp) { 
+        perror("Error: Can't find the output file")
+        // Exits the program
+        return 1;
+    }
+
+    char buffer[500];
+
+    // Get the line
+    while (fgets(buffer, sizeof(buffer), inputFileName)) {
+        // *line points to an array with no trailing white spaces, and no null terminator
+        char *line = removeWhiteSpace(buffer);
 
 
+
+    }
+
+    // Need to create node for the head of the linked list 
+
+    fclose(fp);
+    fclose(fp_out);
 
     return 0;
-
 }
 
 
@@ -76,5 +146,12 @@ int main(int argc, char *argv[]) {
         - Assume everyone has a first and last name 
         - every line ends in a new line character 
         - WE NEED TO HANDLE CORNER CASES 
-2. READ in the input file using: fgets = RETURNS A STRING (NULL TERMINATED) FOR EACH LINE IN THE FILE. A LINE CORRESPONDS TO A STUDENT 
-3. 
+2. READ in the input file using: fgets = RETURNS A STRING (NULL TERMINATED) FOR EACH LINE IN THE FILE. A LINE CORRESPONDS TO A STUDENT. Print error message to text file if does not follow specified format 
+3. We are using struct. so that means the students will be connected to one another via Nodes. So we need a method to take the input data, and create a node with it. 
+4. Ok so we take the data, create a node, and then add it to the linked list. After that is all done, we can iterate through the linked list, and print the data for each list on a line. 
+5. Since we use fgets to create a string from each line of data, we need to parse this string somehow to assign the correct values to the variables in the struct node. 
+    - For Examnple: For Mike Johnson 3.125 D, We need to assign fName: Mike, lName: Johnson, gpa: 3.125, link: link -> link;
+    - Use the white space to parse the string ? and treat the number as a string and then convert it when printing it ? 
+
+    - strtok will replace every delimeter with a new line character. Is this what we want ? 
+
