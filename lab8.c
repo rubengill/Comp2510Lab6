cc179dd6271a1db59c66e939fa2e8782d0b48ad4
Ruben Gill <rgill288@my.bcit.ca>
2023-11-22 20:20:05 -0800

    Added more code to main method in lab 8

--- lab8.c
+++ lab8.c
@@ -2,7 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Add the two numbers
+void logicalLadder(char *ptrNumOne, char *ptrNumTwo, size_t nbytes) {
+    char tempOne[nbytes];
+    char tempTwo[nbytes];
 
+    // Copy the command line numbers into the char arrays since we do not know the type 
+    memcpy(numOne, ptrNumOne, nbytes);
+    memcpy(numTwo, ptrNumTwo, nbytes);
+
+    // Get bit represenation of numbers
+    long numOne = atol(tempOne);
+    long numTwo = atol(tempTwo);
+
+}
+
+// Use bidwitdth to detect overflow 
+detectOverFlow() {
+
+}
 
 
 // Drives the program
@@ -14,17 +32,24 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Command line args
-    int bitwidth;
-    int numOne;
-    int numTwo;
-    int shift;
+    int bitwidth = atoi(argv[1]);
 
     // Check if the bitwidth is the correct number 
-    if (bitWidth != 8 || bitWidth != 16 || bitWidth != 32 || bitWidth != 64) {
+    if (bitWidth != 8 && bitWidth != 16 && bitWidth != 32 && bitWidth != 64) {
         perror("Bitwidth must be: 8, 16, 32 or 64");
         return 1;
     }
 
+    // Calculate the size needed based on the bitwidth
+    size_t nbytes = bitwidth / 8;
+
+    // Shift number
+    int shift = atoi(argv[4]);
+
+    //Preform the addition 
+    logicalLadder(argv[2], argv[3], nbytes);
+    // logicalLadder(&numOne, &numTwo, nbytes);
+
+    return 0;
 }
 
